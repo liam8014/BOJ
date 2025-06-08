cdeb1c22ba8541311e15f5a4bfefb68b5332a88d
NerNoon <66264660+liam8014@users.noreply.github.com>
2025-06-08 17:02:22 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#define IOS ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+int main() {
+    IOS;
+    int n;
+    cin >> n;
+    cout << n - 1946;
+    return 0;
+}
