d55be683e5b9b04ecabd11cbdd3da5fd95c72cea
NerNoon <66264660+liam8014@users.noreply.github.com>
2026-01-04 20:02:38 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 68 ms, Memory: 2552 KB -BaekjoonHub

---
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define IOS ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pii pair<int,int>
+using namespace std;
+pii dir[4] = { {1,0},{0,1},{0,-1},{-1,0} };
+pii hDir[8] = { {2, 1},{-2, 1},{2, -1},{-2, -1},
+                    {1,2},{-1,2},{1,-2},{-1,-2} };
+struct nd{
+    int r, c, w, k;
+};
+int main() {
+    IOS;
+    int K,N,M, ans=-1;
+    cin >> K >> M >> N;
+    vector<vector<int>> v(N,vector<int>(M));
+    vector<vector<int>> vst(N, vector<int>(M, 0));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> v[i][j];
+        }
+    }
+    queue<nd> q;
+    q.push({ 0,0,0,1 });
+    vst[0][0] = true;
+    while (!q.empty()) {
+        int r = q.front().r, c = q.front().c, w = q.front().w, k = q.front().k;
+        if (r == N - 1 && c == M - 1) {
+            ans = w;
+            break;
+        }
+        q.pop();
+        for (auto d : dir) {
+            int nr = r + d.first, nc = c + d.second;
+            if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+            if (v[nr][nc]) continue;
+            if (vst[nr][nc] & 1 << k) continue;
+            vst[nr][nc] |= 1 << k;
+            q.push({ nr,nc,w + 1, k});
+        }
+        if (k - 1 < K) {
+            for (auto d : hDir) {
+                int nr = r + d.first, nc = c + d.second;
+                if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+                if (v[nr][nc]) continue;
+                if (vst[nr][nc] & 1 << (k+1)) continue;
+                vst[nr][nc] |= 1 << (k + 1);
+                q.push({ nr,nc,w + 1, k+1 });
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
