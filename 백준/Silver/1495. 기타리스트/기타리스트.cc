e7a224517a93d088590ad05d01db2461d7e1a1a7
NerNoon <66264660+liam8014@users.noreply.github.com>
2026-02-12 19:25:55 +0900

    [Silver I] Title: 기타리스트, Time: 4 ms, Memory: 2948 KB -BaekjoonHub

---
+++ 백준/Silver/1495. 기타리스트/기타리스트.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#define ll long long
+#define IOS ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+int N, S, M;
+int main() {
+    cin >> N >> S >> M;
+    vector<set<int>> dp(N+1);
+    dp[0].insert(S);
+    for (int i = 1; i <= N; i++) {
+        int volAmt; cin >> volAmt;
+        for (const auto& d : dp[i - 1]) {
+            if (d+volAmt <=M) {
+                dp[i].insert(d + volAmt);
+            }
+            if (d - volAmt >= 0) {
+                dp[i].insert(d - volAmt);
+            }
+        }
+    }
+    if(!dp[N].empty()) cout << *(--dp[N].end());
+    else cout << -1;
+    return 0;
+}
